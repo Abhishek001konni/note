d26d2af53aa678727a60ea492ca767da68fd3b19
Abhishek001konni <Abhishek001konni@gmail.com>
2025-01-21 03:23:36 +0000

    server: init express

---
+++ server/app.js
@@ -0,0 +1,33 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // render the error page
+  res.status(err.status || 500).json({
+    error: {
+      message : err.message,
+      status: err.status || 500
+
+    }
+});
+});
+
+module.exports = app;
