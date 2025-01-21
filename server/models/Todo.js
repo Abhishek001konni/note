e36944c74c306724ecab8e616f11578abe254f5b
Abhishek001konni <Abhishek001konni@gmail.com>
2025-01-20 22:51:15 -0500

    server: models: Define models for Note, Todo, and Paste

---
+++ server/models/Todo.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const todoSchema = new mongoose.Schema({
+    task: { type: String, required: true},
+    createdAt: { type: Date, default: Date.now},
+    completed: { type: Boolean, default: false},
+    updateAt: { type: Date, default: Date.now}
+});   
+
+module.exports = mongoose.model('Todo', todoSchema);
