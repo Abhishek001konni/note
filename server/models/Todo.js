49596473d0145dc1b1b284dee16dba4819ac9f51
Abhishek001konni <Abhishek001konni@gmail.com>
2025-01-24 22:58:20 -0500

    server/todo: Implement CRUD operations for Todo API

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 const todoSchema = new mongoose.Schema({
-    task: { type: String, required: true},
-    createdAt: { type: Date, default: Date.now},
-    completed: { type: Boolean, default: false},
-    updateAt: { type: Date, default: Date.now}
-});   
+    task: { type: String, required: true, trim: true, maxlength: 200},
+    completed: { type: Boolean, default: false },
+
+}, { timestamps: true});
+
+
+
 
 module.exports = mongoose.model('Todo', todoSchema);
