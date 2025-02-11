6d45075b48ddfabce7e9fb9ee55b135f53ecfb14
Abhishek001konni <Abhishek001konni@gmail.com>
2025-02-11 04:35:25 +0000

    client: Add signup and login page(need to edit commit or split )

---
+++ client/pages/login.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref} from 'vue';
+const runtimeConfig = useRuntimeConfig();
+const apiURL  = runtimeConfig.public.apiBase;
+const email = ref('');
+const password = ref('');
+
+const login = async () => {
+        const response = await fetch (`${apiURL}api/auth/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: email.value,
+                password: password.value,
+            })
+        })
+        const data = await response.json();
+    }   
+
+
+</script>
+
+<template>
+    <div>
+        <h1>Login</h1>
+        <form @submit.prevent="login">
+            <input v-model="email" type="email" placeholder="Email" />
+            <input v-model="password" type="password" placeholder="Password" />
+            <button>Login</button>
+        </form>
+    </div>
+</template>
---
+++ client/pages/register.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+const email = ref('');
+const password = ref('');
+const runtimeConfig = useRuntimeConfig();
+const apiURL = runtimeConfig.public.apiBase;
+
+const register = async () => {
+    const response = await fetch(`${apiURL}api/auth/signup`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: email.value,
+            password: password.value,
+        })
+    })
+    const data = await response.json();
+}
+
+</script> 
+
+
+<template>
+<div>
+    <h2>Register</h2>
+    <form @submit.prevent="register">
+        <input v-model="email" type="email" placeholder="Email" />
+        <input v-model="password" type="password" placeholder="Password" />
+        <button>Register</button>
+    </form>
+</div>
+
+</template>
