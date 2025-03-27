2b620a409029dcf4892c79a98fb54ff9d35bcc14
Abhishek001konni <Abhishek001konni@gmail.com>
2025-03-27 16:34:20 -0400

    client: components: Create Base Navigation component

---
+++ components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+    <nav class="bg-red-500 p-4">
+      <ul class="flex space-x-6 text-white">
+        <li><NuxtLink to="/" class="hover:underline">Home</NuxtLink></li>
+        <li><NuxtLink to="/notes" class="hover:underline">Notes</NuxtLink></li>
+        <li><NuxtLink to="/todo" class="hover:underline">Todos</NuxtLink></li>
+        <li><NuxtLink to="/paste" class="hover:underline">Pastes</NuxtLink></li>
+      </ul>
+    </nav>
+  </template>
