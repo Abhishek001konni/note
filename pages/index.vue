be38d0be45350668bf064f7bf5581ad8d916449a
Abhishek001konni <Abhishek001konni@gmail.com>
2025-03-27 16:28:57 -0400

    client: pages: Add initial Index page

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+      <Navbar />
+    </div>
+    <div class="p-6">
+      <h1 class="text-3xl font-bold text-gray-800 bg-blue-500 rounded">Welcome to My App</h1>
+      <p class="mt-2 text-gray-600">A note-taking, todo, and paste-sharing app.</p>
+    </div>
+  </template>
